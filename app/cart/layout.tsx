f5faed743cbf70840d33d2d2ce7e9bc330f27a29
Aireddy <2443041@cognizant.com>
2025-11-19 17:32:39 +0530

    Revert "shopping cart feature code"
    This reverts commit c308436a17098f844844c5877c2d3e85a417cf91.

--- app/cart/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CartProvider } from '@/src/context/CartContext';
-
-export default function CartLayout({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
